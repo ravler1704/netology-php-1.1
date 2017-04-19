a925e22f4f827d40dddb3c19a15986fe9758f03a
Alexandr Raskatov <avr1704@yandex.ru>
2017-04-19 18:22:13 +0400

    Update animals.php

--- animals.php
+++ animals.php
@@ -4,53 +4,42 @@ Error_reporting(E_ALL);
 $continents = array (
 "Africa" => array("Mammuthus columbi", "Diceros bicornis", "Panthera leo", "Cephalophinae", "Elephantidae"),
 "Russia" => array("Canidae", "Lepus europaeus", "Sciurus", "Sus scrofa", "Erinaceidae"),
-"America" => array("Ovis nivicola", "Lynx rufus", "Rangifer tarandus", "Canis latrans", "Ursus arctos horribilis")
+"America" => array("Ovis nivicola", "Lynx rufus", "Rangifer tarandus", "Canis latrans", "Ursus arctos")
 );
 
 echo "<h1>Исходный массив</h1>";
-
 foreach ($continents as $continent_name => $continent) {
 	echo "<h2>{$continent_name}</h2>";
 		foreach ($continent as $animals) {
 			echo "<p>{$animals}</p>";
-
-			
 		}
 }
 
 echo "<h1>Массив с животными из 2 слов</h1>";
-
+$animals_2_words_array = array();
 foreach ($continents as $continent_name => $continent) {
 	echo "<h2>{$continent_name}</h2>";
 		foreach ($continent as $animals) {
-			echo "<p>{$animals}</p>";
-				$animals_explode = explode(" ", $animals);
-				echo count($animals_explode);
-
-			
-		}
-}
-
-
-echo "<h1>ТЕСТ</h1>";
-
-foreach ($continents as $continent_name => $continent) {
-	echo "<h2>{$continent_name}</h2>";
-		foreach ($continent as $animals) {
-			
-				$animals_explode = explode(" ", $animals);//разделяем строку $animals на массив $animals_explode
-				$counter = count($animals_explode);//считаем сколько элементов в массиве $animals_explode
-				//echo $counter;
-				if ($counter == 2) {//если 2 элемента
-				$animals_explode_impode = implode(" ", $animals_explode);//объединяем их в строку
-				var_dump ($animals_explode_impode);//получаем 3 строки
-				
+				$animals_explode_space = explode(" ", $animals);
+				if (count ($animals_explode_space) == 2) {
+					$animals_2_words = $animals;
+				echo "<p>{$animals_2_words}</p>";
+				$animals_2_words_array[] = $animals_2_words;
 				}
-				
-				
-			
 		}
 }
 
+echo "<h1>Перемешанные животные</h1>";
+$counter = count ($animals_2_words_array);
+for ($i = 0; $i < $counter; $i++) {
+	$animals_2_words_array_explode = explode(" ", $animals_2_words_array[$i]);//раздел¤м животное [i] на два слова
+	$animal_first[] = $animals_2_words_array_explode[0]; //первое слово в массив $animal_first[]
+	$animal_second[] = $animals_2_words_array_explode[1]; //второе слово в массив $animal_second[]	
+}
+shuffle($animal_second);
 
+for ($i = 0; $i < $counter; $i++) {
+	echo $animal_first[$i]." ".$animal_second[$i]."<br/>";
+	$mixed_pet[] = $animal_first[$i]." ".$animal_second[$i]."<br/>";
+}
 ?>
